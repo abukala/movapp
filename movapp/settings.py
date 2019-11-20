86ff2d57f99e7546e8ed5287b1dbbf51b15efb17
Andrzej Bukała <a.bukala@negesi.pl>
2019-11-20 18:49:38 +0100

    Added docker and docker-compose files

--- movapp/settings.py
+++ movapp/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'movapp',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
